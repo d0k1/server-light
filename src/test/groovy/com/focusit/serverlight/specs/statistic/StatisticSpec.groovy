0972ca4f4b153358a231a1419ddc67136afbf391
dOkI <denis.kirpichenkov@gmail.com>
2017-12-21 07:43:56 +0500

    mbean test

--- src/test/groovy/com/focusit/serverlight/specs/statistic/StatisticSpec.groovy
+++ src/test/groovy/com/focusit/serverlight/specs/statistic/StatisticSpec.groovy
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit
 class StatisticSpec extends Specification {
 
     def "statistics can store and retrieve data"(){
-        Statistic stat = new Statistic("com.focusit.serverlight.statistics", "5sec", new Date().getTime(), 2, TimeUnit.SECONDS, 60, TimeUnit.SECONDS);
+        Statistic stat = new Statistic("com.focusit.serverlight.statistics", "2sec", new Date().getTime(), 2, TimeUnit.SECONDS, 60, TimeUnit.SECONDS);
 
         when:
         stat.addData(3.14);
@@ -18,6 +18,27 @@ class StatisticSpec extends Specification {
         Thread.sleep(3000);
 
         stat.addData(6.14);
+
+        Map<Long, IntervalStatistics> data = stat.getRaw();
+        then:
+        data.size()==2
+    }
+
+    def "statistics can register mbean"(){
+        Statistic stat = new Statistic("com.focusit.serverlight.statistics", "2sec", new Date().getTime(), 2, TimeUnit.SECONDS, 60, TimeUnit.SECONDS);
+        Statistic stat2 = new Statistic("com.focusit.serverlight.statistics", "1sec", new Date().getTime(), 1, TimeUnit.SECONDS, 60, TimeUnit.SECONDS);
+
+        when:
+        stat.registerMBean();
+        stat2.registerMBean();
+
+        stat.addData(3.14);
+        stat2.addData(3.24);
+
+        Thread.sleep(3000);
+
+        stat.addData(6.14);
+        stat2.addData(6.24);
 
         Map<Long, IntervalStatistics> data = stat.getRaw();
         then:
